2b1431a30c64a7c08c3a9ed9d4479f69cc3aa23e
Leonardo Melo Moreira <leonardomm@gmail.com>
2015-04-28 10:20:54 -0300

    correções banner.js

--- js/banner.js
+++ js/banner.js
@@ -2,6 +2,7 @@
 
 //Create the Banner Object
 function Banner(){
+	"use strict";
 	//Creates the object properties
 	this.imagens = new Array();
 	this.largura = new Number();
@@ -11,22 +12,22 @@ function Banner(){
 	//Assigns the width of the images
 	this.setLargura = function(num){
 		this.largura = num;
-	}
+	};
 	
 	//Add url to the image array
 	this.setImagens = function(url){
 		this.imagens.push(url);
-	}
+	};
 	
 	//Assigns the box that will receive the list of banners
 	this.setBox = function(){
 		this.boxListaBanner = document.getElementById("box-banner-lista");
-	}
+	};
 	
 	//Assigns the width of the box that will receive the banners
 	this.setTamBox = function(){
 		this.tamBox = parseInt(this.boxListaBanner.style.width.replace("px",""));
-	}
+	};
 	
 	//Method that carries the banner and starts the automatic transition
 	this.carrega = function(){
@@ -43,31 +44,26 @@ function Banner(){
 		} catch(erro){
 			console.log("Erro: "+erro);
 		}
-	}
+	};
 	
 	//Method that moves the banners based on the parameter "direction". ">" forward and "<" back
 	this.move = function(direcao){
 		var posLista = parseInt(this.boxListaBanner.style.marginLeft.replace("px",""));
 		var tamBox;
 		var novaPosicao = "";
-		switch(direcao){
-			case ">":
-				if((posLista - this.largura) < ((0 - this.tamBox) + this.largura)){
-					novaPosicao = 0;
-				} else {
-					novaPosicao = posLista - this.largura;
-				}
-			break;
-			case "<":
-				if((posLista + this.largura) > 0){
-					novaPosicao = (0 - this.tamBox) + this.largura;
-				} else {
-					novaPosicao = posLista + this.largura;
-				}
-			break;
+		if(direcao === ">"){
+			if((posLista - this.largura) < ((0 - this.tamBox) + this.largura)){
+				novaPosicao = 0;
+			} else {
+				novaPosicao = posLista - this.largura;
+			}
+		} else if(direcao === "<"){
+			if((posLista + this.largura) > 0){
+				novaPosicao = (0 - this.tamBox) + this.largura;
+			} else {
+				novaPosicao = posLista + this.largura;
+			}
 		}
 		this.boxListaBanner.style.marginLeft = novaPosicao + "px";
-		console.log(this.boxListaBanner.style.width);
-		console.log(this.boxListaBanner.style.marginLeft);
-	}
-}
+	};
+};
